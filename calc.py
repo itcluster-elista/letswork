da8ac0e4006dca52321a17dd64491eecf80edd92
bmwmist <namsr007@gmail.com>
2023-04-18 19:11:10 +0300

    i wrote

--- calc.py
+++ calc.py
@@ -4,3 +4,9 @@ opr = input("Enter operation: ") #Джилеева Сяхля
 
 if opr = '+':
 	print(x + y) #Nominhan Budenkaev
+elif opr == "-":
+	print(x-y)
+elif opr == "*":
+	print(x*y)	
+elif opr == "/": #Kamilla Tahtenova
+	print(x/y)	
