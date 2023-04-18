a22f75eecd0b45299b79d3872eb89fd35b392b5b
Danil Z <paradizeblag@gmail.com>
2023-04-18 19:14:18 +0300

    I

--- calc.py
+++ calc.py
@@ -2,5 +2,13 @@ x = float(input("Enter x: ")) #Темир Саксыков
 y = float(input("Enter y: ")) #Ahmeev Ais
 opr = input("Enter operation: ") #Джилеева Сяхля
 
-if opr = '+':
-	print(x + y) #Nominhan Budenkaev
+if opr == '+': #Nominhan Budenkaev
+	print(x + y) 
+elif opr == '-': 
+	print(x - y)
+elif opr == '*': #Temir
+	print(x * y)
+elif opr == '/': #Kamila
+	print(x / y)
+else: #Aldar
+	print("Incorrect option")
