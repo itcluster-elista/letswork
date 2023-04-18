06eec2f37c1145158135a5cb6d180e96e61eab92
Ivan <stich634@mail.ru>
2023-04-18 18:24:49 +0300

    put new number in maks

--- group4.py
+++ group4.py
@@ -2,4 +2,5 @@ limit = int(input("Enter limit: ")) # Eugen Bembeev
 maks = 0
 
 for i in range(limit): 
-	if i % 4 ==  0 and i > maks:  #Chemidov Aldar
+	if i % 4 ==  0 and i > maks: #Chemidov Aldar
+		maks = i  #Serghey Eneev
