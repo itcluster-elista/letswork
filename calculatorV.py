85b7a2abd617b0cb7c8ae0947498418f44792d80
Ivan <stich634@mail.ru>
2023-04-20 14:58:06 +0300

    I made a calculatorV

---
+++ calculatorV.py
@@ -0,0 +1,11 @@
+a = int(input())
+b = int(input())
+c = input()
+if c == '+':
+	print(f'{a} + {b} = {a+b}')
+elif c == '-':
+	print(f'{a} - {b} = {a-b}')
+elif c == '*':
+	print(f'{a} * {b} = {a*b}')
+elif c == '/':
+	print(f'{a} / {b} = {a/b}')
