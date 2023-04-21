a451023280c6a47bca4b07927d0172a32068ba21
Mingian <mingianbovaev9@gmail.com>
2023-04-21 18:32:49 +0300

    I added new code to file

--- aita.py
+++ aita.py
@@ -1,2 +1,15 @@
 print ("Hello world!")
 # a=6, b=3, f=10
+class Class1(object): 
+	def __init__(self, value):
+		self.__var = value 
+	def getVar(self):
+		return self.__var
+	def setVar(self, value):
+		self.__var = value 
+		
+c1 = Class1(5)
+print(c1.getVar())
+
+c1.setVar(35)
+print(c1.getVar())
